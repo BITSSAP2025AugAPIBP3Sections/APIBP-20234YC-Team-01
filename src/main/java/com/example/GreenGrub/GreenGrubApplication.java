74c8048bb3639715978bb11466843a9a4e9875ab
Antika Burman <antika.burman@sap.com>
2025-09-25 20:48:00 +0530

    Initial commit

---
+++ src/main/java/com/example/GreenGrub/GreenGrubApplication.java
@@ -0,0 +1,13 @@
+package com.example.GreenGrub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenGrubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenGrubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GreenGrub/GreenGrubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GreenGrub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenGrubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
