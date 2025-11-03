ff8e6f8843f9bea144249e2d15e4806f76cdd8f3
I528787 <abhishek.gupta23@sap.com>
2025-11-03 19:48:19 +0530

    db setup

--- src/main/java/com/example/GreenGrub/GreenGrubApplication.java
+++ src/main/java/com/example/GreenGrub/GreenGrubApplication.java
@@ -2,15 +2,9 @@ package com.example.GreenGrub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-
+@SpringBootApplication
 public class GreenGrubApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GreenGrubApplication.class, args);
 	}
-
 }
